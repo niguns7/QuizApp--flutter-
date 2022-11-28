fe91eb7c2c0e6928952195272f254ccd7890aeb9
niguns7 <nirja767@gmail.com>
2022-11-28 16:45:25 +0545

    styled little bit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main () {
-  runApp(MyApp()); //main function to show content on the screen
-}
+//void main () {
+//  runApp(MyApp()); //main function to show content on the screen
+//}
+//context holds some metadata about this widgid
+//every widgit are the dart classes which at the end have build methode
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget build (BuildContext context){
-    return MaterialApp(home: Text('Hello!!'),);
+  //flutter need to extends either stateless or statefull widgits
+  Widget build(BuildContext context) {
+    //build is the methode which flutter eill call to draw something tot he screen
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("first app"),
+        ),
+        body: Text("Hello welcome to flutter"),
+      ),
+    );
   }
 }
