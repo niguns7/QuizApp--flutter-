47438aa5dd458d82865fe63deaae066e487281aa
niguns7 <nirja767@gmail.com>
2022-11-29 13:47:43 +0545

    button added

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,35 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   //flutter need to extends either stateless or statefull widgits
+  @override
   Widget build(BuildContext context) {
     //build is the methode which flutter eill call to draw something tot he screen
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("first app"),
+          title: Text('first app'),
         ),
-        body: Text("Hello welcome to flutter"),
+        body: Column(
+          children: [
+            Text("the question!!"),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 3'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 4'),
+            ),
+          ],
+        ),
       ),
     );
   }
