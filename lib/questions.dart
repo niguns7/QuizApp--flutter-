516dcf90f3119bd54ee04e220008179f60128cec
niguns7 <nirja767@gmail.com>
2022-11-30 13:51:12 +0545

    styling added

--- lib/questions.dart
+++ lib/questions.dart
@@ -1,11 +1,21 @@
+//this file is for questions
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  final String questionText; //it could be changed internally so the final is added which tells dart that is will never change
+  final String
+      questionText; //it could be changed internally so the final is added which tells dart that is will never change
 
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(15),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
