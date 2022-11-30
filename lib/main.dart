1dda88e14c22ebb0bd5f27532b4db6e17ff2d660
niguns7 <nirja767@gmail.com>
2022-11-30 15:39:04 +0545

    mapping an array of objects

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,41 @@ import './answer.dart';
 //every widgit are the dart classes which at the end have build methode
 
 void main() => runApp(MyApp());
+
 //if we pass answerQuestion () that expect result of the function meanwhile in our function is a void type function so we onkly call its name
 class MyApp extends StatefulWidget {
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-
-  var _questionIndex = 0; 
+  var _questionIndex = 0;
 
   var questions = [
-      'what\'s your favourait color?',
-      'what\'s your favourait animal?',
-    ];
-  void _answerQuestion () {
+    {
+      'questionText': 'what\'s your favourait color?',
+      'answers': ['Black', 'Red', 'Blue', 'White']
+    },
+    {
+      'questionText': 'what\'s your favourait animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+    },
+    {
+      'questionText': 'what\'s your favourait sport?',
+      'answers': ['Football', 'Basketball', 'Cricket', 'Tenis']
+    },
+  ];
+  void _answerQuestion() {
     setState(() {
-      _questionIndex= _questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-   
+
     print(_questionIndex);
   }
+
   //flutter need to extends either stateless or statefull widgits
   @override
   Widget build(BuildContext context) {
-  
     //build is the methode which flutter eill call to draw something tot he screen
     return MaterialApp(
       home: Scaffold(
@@ -44,7 +53,7 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(
-              questions[_questionIndex],
+              questions[_questionIndex]['questionText'], //passing a key
             ),
             Answer(_answerQuestion),
             Answer(_answerQuestion),
