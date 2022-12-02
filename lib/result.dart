281335ffb5e87d0571bcc17020ceb8bf3e667d30
niguns7 <nirja767@gmail.com>
2022-12-02 21:13:59 +0545

    added condition for result

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
 class Result  extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase{
+    var resultText = 'you did it!';
+    if (resultScore <= 18) {
+        resultText = 'you are awesom';
+    } else if (resultScore <= 12) {
+        resultText = 'you are preety good';
+    }else if (resultScore <= 8) {
+        resultText = 'you are.... strange';
+    }else{
+      resultText = 'you are bad';
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Quiz is finished'),
+    return Center(child: Text(resultPhrase, style: TextStyle(fontSize: 36,fontWeight: FontWeight.bold),),
         );
   }
 }
